1896841860698f1919ed1c0fccb23420e8f768a2
Daniel Garcia <drgarcia@mymail.mines.edu>
2020-05-21 22:22:14 -0600

    Separated field_sesh.html javascript into VisualizationHandler.js, Conference.js, and left the rest of the code in field_sesh.html within the last <script></script> tag

---
+++ Conference.js
@@ -0,0 +1,167 @@
+/**
+ * Conference Class
+ */
+class Conference {
+    //constructor (must include all fields atm)
+    constructor(conf_name, conf_index, x, y, conf_field, conf_color, submission_deadline, decision_deadline, conferenceStart_date, conferenceEnd_date, notification_deadline, conf_location, radius, inner_radius, outer_radius, start_angle, end_angle, ring, arc) {
+        //var. having to do with conference info.
+        this.conf_name = conf_name;
+        this.conf_index = conf_index;
+        this.conf_field = conf_field;
+        this.conf_color = conf_color;
+
+        //var. having to do with conference vis.
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.inner_radius = inner_radius;
+        this.outer_radius = outer_radius;
+        this.start_angle = start_angle;
+        this.end_angle = end_angle;
+
+         //var. having to do with both conf. vis. & conf. info.
+        this.submission_deadline = submission_deadline;
+        this.decision_deadline = decision_deadline;
+        this.conferenceStart_date = conferenceStart_date;
+        this.conferenceEnd_date = conferenceEnd_date;
+        this.notification_deadline = notification_deadline;
+        this.conf_location = conf_location;
+
+        //conf. shapes
+        this.ring = ring;
+        this.arc = arc;
+    }
+
+     /**
+     * showDetails
+     * @params document, conference
+     * document: used to append created elements to html
+     * conference: used to grab data necessary to populate created elements
+     *
+     * showDetails is in charge of displaying the data corresponding to each conference.
+     * It is a base template and used for all conferences, as they should all have the same
+     * data fields.
+     */
+    showDetails(document, conference) {
+        // new elements are added dynamically on conference visualization click. This means that objects
+        // need to be deleted and re-added each time a new conference is clicked. This array and fo r loop
+        //
+        // handles conference descriptive element deletions
+        var details_element_ids = ["conferenceName", "mainParagraph", "deleteButton"];
+        for (var i = 0; i < details_element_ids.length; i++) {
+            if (document.getElementById(details_element_ids[i])){
+                var elem = document.getElementById(details_element_ids[i]);
+                elem.remove();
+            }
+        }
+
+        //create a new container that will appear on the left side of the screen
+        var container2 = document.createElement("div");
+        container2.setAttribute("style","float: left;");
+        document.body.appendChild(container2);
+
+        var container3 = document.createElement("div2");
+        container3.setAttribute("style","float: absolute;");
+        container3.style.position = "absolute";
+        container3.style.left = 1025 + 'px';
+        container3.style.top = 175 +'px';
+        document.body.appendChild(container3);
+
+        // Header element, will contain Conference Name
+        var h1 = document.createElement('h1');
+        h1.id = "conferenceName";
+        h1.textContent = conference.conf_name;
+        var left = document.clientWidth/2;
+        var top = document.clientHeight/2;
+        //h1.style.left = left;
+        //h1.style.top = top;
+        //h1.setAttribute("style", "position: left; z-index: 1; left: "+left+"; top: "+top+"; width:200px; height:20px;");
+        //h1.style.zIndex = 1;
+        container2.appendChild(h1);
+        //container2.appendChild(h1);
+
+        // Paragraph element, unknown what this will contain
+        var p1 = document.createElement('p1');
+        p1.id = "mainParagraph";
+        p1.setAttribute("style", "position: absolute; z-index: 1; left: "+left+"; top: "+top+"; width:190px; height:100px;");
+        p1.textContent = "Conference Topic: " + conference.conf_field + "\r\nLocation: " + conference.conf_location + "\r\nSubmission Deadline: " + conference.submission_deadline + "\r\nNotification Deadline: " + conference.notification_deadline + "\r\nFinal Version Due: " + conference.decision_deadline;
+        p1.setAttribute('class', 'note');
+        p1.setAttribute('vertical-align', 'baseline');
+        container2.appendChild(p1);
+
+        var deleteButton = document.createElement("button");
+        deleteButton.innerHTML = "Delete Conference";
+        deleteButton.id = "deleteButton";
+        deleteButton.width = "20px";
+        deleteButton.height = "100px";
+        deleteButton.top = "100px";
+        deleteButton.left = "20px";
+        container3.appendChild(deleteButton);
+
+        deleteButton.onclick = function() {
+            conference.delConf();
+        }
+    }
+
+    /**
+     * createConfRing
+     * @params svg, document, conference, x, y, radius, color, ring_size
+     * svg: svg object, used to add created shapes into svg
+     * x, y: Cartesian coordinates of the center of circle or arc to be created on the canvas
+     * radius: the radius of the circle or arc being created
+     * start_angle, end_angle: used to define start and end points if arc is being created
+     * color: color of circle or arc
+     * ring_size: width of stroke for ring
+     *
+     * Creates conference ring
+     */
+    //Create Conference Visualizations (ring & arc), and add click handlers. TODO: SHOULD CLICK HANDLERS BE TAKEN CARE OF IN THE SAME FUNCTION???
+    static createConfRing(svg, document, conference, x, y, radius, color, ring_size) {
+        //init. and append new conference ring to svg
+        var new_conf_ring = svg.append("circle").attr("cx", x).attr("cy", y)
+                                                .attr("r", radius)
+                                                .attr("stroke", color).attr("stroke-width", ring_size)
+                                                .style("fill", "none")
+                                                .on("click", function() {
+                                                    conference.showDetails(document, conference);
+                                                    selected_conf_index = conference.conf_index;
+                                                });
+        return new_conf_ring;
+    }
+
+    /*
+     * createConfArc
+     * @params: svg, document, conference, x, y, radius, start_angle, end_angle, color, arc_size
+     * svg: svg object, used to add created shapes into svg
+     * x, y: Cartesian coordinates of the center of circle or arc to be created on the canvas
+     * radius: the radius of the circle or arc being created
+     * start_angle, end_angle: used to define start and end points if arc is being created
+     * color: color of circle or arc
+     * arc_size: width of stroke for arc
+     * Creates Conference arc indicating all deadlines
+     */
+    static createConfArc(svg, document, conference, x, y, radius, start_angle, end_angle, color, arc_size) {
+        //init. and append new conference arc to svg
+        var newly_built_arc = d3.svg.arc().innerRadius(radius - (arc_size/2))
+                                              .outerRadius(radius + (arc_size/2))
+                                              .startAngle(start_angle)
+                                              .endAngle(end_angle);
+
+        var new_conf_arc = svg.append("path").attr("d", newly_built_arc).attr("transform", "translate("+x+", "+y+")").style("fill", color)
+                                                .on("click", function() {
+                                                   conference.showDetails(document, conference);
+                                                    selected_conf_index = conference.conf_index;
+                                                });
+        return new_conf_arc;
+    }
+
+    /*
+     * createConfVisuals
+     *
+     * Uses the two previous function to handle all Conference visualization creation
+     */
+    static createConfVisuals(svg, document, conf, x, y, radius, start_angle, end_angle, conf_color, ring_size, arc_size) {
+        conf.ring = Conference.createConfRing(svg, document, conf, x, y, radius, conf_color, ring_size);
+        conf.arc = Conference.createConfArc(svg, document, conf, x, y, radius, start_angle, end_angle, conf_color, arc_size);
+    }
+}
---
+++ VisualizationHandler.js
@@ -0,0 +1,195 @@
+/*
+* visualizationHandler
+*
+* Class handles all necessary functions needed to be performed on the macro visualization
+*/
+class VisualizationHandler {
+   /*
+    * constructor
+    *
+    * @params svg, document, x, y, conferences, num_confs, names, fields, colors, submission_deadlines, notification_deadlines, decision_deadlines
+    * svg: svg
+    * document: used in creation of Conference object
+    * x: where visualization is centered on the x axis
+    * y: where visualization is centered on the y axis
+    * conferences: array of Conference objects inputted into visualization on start (currently is an empty array, as we have nothing stored)
+    * num_confs: number of conferences
+    * names: conference names
+    * fields: conference fields
+    * colors: conference visualization colors
+    * submission_deadlines: conference submission deadlines
+    * notification_deadlines: conference notification deadlines
+    * decision_deadlines: conference decision deadlines
+    *
+    * This method handles the start-up creation of our visualization. As of now it takes lists of all conference info in order using
+    * separate arrays. For example, for the creation of two conferences you would have names_array = ["name_1", "name_2"],
+    * fields_array = ["field_1", "field_2"], etc. If it is seen that the better way of creating conferences is using a two dimensional
+    * array of conference information (i.e. [["name_1", "field_1"]["field_1", "field_2"]]), then necessary adjustments will be made.
+    * If this change is made it should be noted that updating info arrays IS ESSENTIAL
+    */
+   constructor(svg, document, x, y, conferences, num_confs, names, fields, colors, submission_deadlines, decision_deadlines, conferenceStart_dates, conferenceEnd_dates, notification_deadlines, conf_locations,) {
+       this.svg = svg;
+       this.document = document;
+       this.x = x;
+       this.y = y;
+
+       this.conferences = conferences;
+       this.num_confs = num_confs;
+       this.names = names;
+       this.fields = fields;
+       this.colors = colors;
+       this.submission_deadlines = submission_deadlines;
+       this.decision_deadlines = decision_deadlines;
+       this.conferenceStart_dates = conferenceStart_dates;
+       this.conferenceEnd_dates = conferenceEnd_dates;
+       this.notification_deadlines = notification_deadlines;
+       this.conf_locations = conf_locations;
+       this.radii = [];
+       this.inner_radii = [];
+       this.outer_radii = [];
+       this.start_angles = [];
+       this.end_angles = [];
+       this.rings = [];
+       this.arcs = [];
+       this.self = this;
+
+       // smallest conference visualization radius
+       this.curr_radius = 200;
+
+       // variables necessary for visual creation
+       var spacing = 20;
+       var ring_size = 3;
+       var arc_size = 10;
+
+       // populate conferences array & fill other empty arrays
+       for (var i = 0; i < num_confs; i++) {
+           // add math related translations for arc creation
+           this.radii.push(this.curr_radius);
+           this.inner_radii.push(this.curr_radius - arc_size/2);
+           this.outer_radii.push(this.curr_radius + arc_size/2);
+           this.start_angles.push(((submission_deadlines[i])/365) * (2*Math.PI));
+           this.end_angles.push(((decision_deadlines[i])/365) * (2*Math.PI));
+
+   // handle single conference creation
+   this.addConference(svg, document, num_confs, this.names[i], i, this.x, this.y, this.fields[i], this.colors[i],
+       this.submission_deadlines[i], this.decision_deadlines[i], this.conferenceStart_dates[i], this.conferenceEnd_dates[i], this.notification_deadlines[i], this.conf_locations[i],
+       this.curr_radius, this.inner_radii[i], this.outer_radii[i], this.start_angles[i], this.end_angles[i], ring_size, arc_size,
+       this.conferences, this.rings, this.arcs);
+
+           // update curr_radius
+           this.curr_radius += spacing;
+       }
+
+   }
+
+   // handles addition of all conference information
+   addConference(svg, document, num_confs, name, i, x, y, field, color, submission_deadline, decision_deadline, conferenceStart_dates, conferenceEnd_dates, notification_deadlines, conf_locations, curr_radius, inner_radius, outer_radius,
+                 start_angle, end_angle, ring_size, arc_size, conferences, rings, arcs) {
+       console.log("Added conference");
+       console.log(name, i, x, y, field, color, submission_deadline, decision_deadline, conferenceStart_dates, conferenceEnd_dates, notification_deadlines, conf_locations,
+                                   curr_radius, inner_radius, outer_radius, start_angle, end_angle, null, null);
+
+       var conf = new Conference(name, i, x, y, field, color, submission_deadline, decision_deadline, conferenceStart_dates, conferenceEnd_dates, notification_deadlines, conf_locations,
+           curr_radius, inner_radius, outer_radius, start_angle, end_angle, null, null);
+
+       //Create conferences visuals and add to visualization handler arrays 'rings' and 'arcs's
+       Conference.createConfVisuals(svg, document, conf, conf.x, conf.y, conf.radius, conf.start_angle, conf.end_angle, conf.conf_color, ring_size, arc_size);
+       rings.push(conf.ring);
+       arcs.push(conf.arc);
+
+       conferences.push(conf);
+       return num_confs + 1;
+   }
+
+   delAndUpdate(svg, conference, selected_index, rings, arcs, num_confs, spacing, conferences, document, arc_size) {
+       // remove ring and arc from d3 and svg
+       rings[selected_index].remove();
+       arcs[selected_index].remove();
+
+       // check if you need to update the visualization
+       if (conference.conf_index < (num_confs - 1)) {
+           console.log("Needs updating");
+
+           for (var i = conference.conf_index+1; i < num_confs; i++) {
+               console.log("Currently updating");
+
+               conference = conferences[i];
+
+               //update ring
+               conference.conf_index -= 1;
+               rings[i].attr("r", conference.radius - spacing);
+
+               //update arc
+               // 1) remove current arc
+               arcs[i].remove();
+               arcs[i] = null;
+               // 2) create new, updated arc and re-add to conf object
+               arcs[i] = Conference.createConfArc(svg, document, conference, conference.x, conference.y, conference.radius - spacing,
+                                                         conference.start_angle, conference.end_angle, conference.conf_color, arc_size);
+           }
+       }
+
+       // return updated num_confs
+       return num_confs - 1;
+   }
+
+   // creates line on visualization indicating the day
+   createDateLine(svg, data_handler, now, x, y, r) {
+       //handle date conversion for finding correct angle
+       var converted_date = data_handler.convertDateToNum(now);
+       console.log(converted_date);
+       var date_angle = (converted_date/365) * (2*Math.PI);
+       console.log(date_angle);
+       var newly_built_date_line = d3.svg.line.radial().angle(date_angle).radius(function(d) { return d; } );
+       console.log(newly_built_date_line);
+
+       //MAGIC NUMBER 15 LOOKIN REEEEAAAAAL SHIT
+       var date_line = svg.append("line").attr("x1", x).attr("y1", y)
+                                         .attr("x2", x + (r - 15)*Math.cos(date_angle)).attr("y2", y + (r - 15)*Math.sin(date_angle))
+                                         .attr("stroke-width", 2).attr("stroke", "black");
+       return date_line;
+   }
+   
+   // creates 12 month arcs inside the smallest conference radius to show the user how time is alloted in the visualization
+   createMonthsRing(svg, x, y) {
+       // how month names are displayed, change this array if you would like different names to be shown
+       var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+       // empty arrays to store the 12 created month arcs
+       var month_arcs = [];
+       var month_text_objs = [];
+
+       // math portion to properly define size and spacing of arcs
+       var spacing = (0.05*((2*Math.PI)/12));
+       var arc_length = (((2*Math.PI)/12) - (2*spacing));
+       var start_angle = spacing;
+       var end_angle = arc_length - spacing;
+
+       // begin creation of the 12 month arcs and their accompanying text
+       for (var i = 0; i < 12; i++) {
+            //init. and append new conference arc to svg
+           var newly_built_arc = d3.svg.arc().innerRadius(170)
+                                                 .outerRadius(190)
+                                                 .startAngle(start_angle)
+                                                 .endAngle(end_angle);
+
+           var new_month_arc = svg.append("path").attr("id", months[i] + "_arc").attr("d", newly_built_arc).attr("transform", "translate("+x+", "+y+")").style("fill", "green");
+
+
+
+           //var months_text_arc = svg.append("path").attr("id", "path_text_months").attr("d", "M75,300 A125,125 0 0,1 325,300").attr("fill", "none").style("stroke", "#AAAAAA");
+           var months_text = svg.append("text").attr("id", months[i])
+                                               .attr("x", 20)   //Move the text from the start angle of the arc
+                                               .attr("dy", 18) //Move the text down
+                                               .append("textPath").attr("xlink:href", "#"+months[i]+"_arc").style("text-anchor", "middle").text(months[i]);
+
+           // store the created text and arc (not really necessary as of now, but it's here just in case)
+           month_arcs.push(new_month_arc);
+           month_text_objs.push(months_text);
+
+           // update start and end angles for the next arc to be created
+           start_angle += arc_length + 2*spacing;
+           end_angle += arc_length + 2*spacing;
+       }
+   }
+}
